861a18097e16c596af221de91664085adc3049ca
Nikita <nk.dsn.nv@gmail.com>
2024-03-30 10:16:03 +0300

    add ContextListener

---
+++ src/main/java/com/weather/listeners/ContextListener.java
@@ -0,0 +1,28 @@
+package com.weather.listeners;
+
+import com.weather.utils.ThymeleafUtil;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("templateEngine", ThymeleafUtil.buildTemplateEngine(context));
+
+        JakartaServletWebApplication application = JakartaServletWebApplication.buildApplication(context);
+        context.setAttribute("application", application);
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ServletContextListener.super.contextDestroyed(sce);
+    }
+}
