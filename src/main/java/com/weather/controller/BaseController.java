31f8897cc2a28e37db87aa21e634c9d3a2ee83ef
Nikita <nk.dsn.nv@gmail.com>
2024-03-30 10:15:37 +0300

    add BaseController

---
+++ src/main/java/com/weather/controller/BaseController.java
@@ -0,0 +1,31 @@
+package com.weather.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+public class BaseController extends HttpServlet {
+    protected TemplateEngine templateEngine;
+    protected JakartaServletWebApplication application;
+
+    @Override
+    public void init() throws ServletException {
+        templateEngine = (TemplateEngine) getServletContext()
+                .getAttribute("templateEngine");
+
+        application = (JakartaServletWebApplication) getServletContext().getAttribute("application");
+    }
+
+    public IContext buildIContext(HttpServletRequest req, HttpServletResponse resp) {
+
+        IWebExchange webExchange =  application.buildExchange(req, resp);
+        return new WebContext(webExchange);
+    }
+
+}
